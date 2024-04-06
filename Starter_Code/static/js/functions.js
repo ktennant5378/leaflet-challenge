80756c58b10af4dbecd033edc4c80ed234751c7f
Kenway Tennant <kengtennant@gmail.com>
2024-04-06 15:31:27 -0700

    Added marker functionality - changes color based on depth - changes size based on magnitude

--- Starter_Code/static/js/functions.js
+++ Starter_Code/static/js/functions.js
@@ -1,3 +1,15 @@
+// Function to get color based on depth
+function getColor(depth) {
+    // This is an example. Update it according to your preferred color scheme.
+    return depth > 300 ? '#800026' :
+        depth > 200 ? '#BD0026' :
+            depth > 100 ? '#E31A1C' :
+                depth > 50 ? '#FC4E2A' :
+                    depth > 20 ? '#FD8D3C' :
+                        depth > 10 ? '#FEB24C' :
+                            '#FFEDA0';
+}
+
 function getMarkers(data) {
     let earthquakeArray = data.features.map(earthquake => ({
         coordinates: [earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]],
@@ -8,12 +20,24 @@ function getMarkers(data) {
 
     console.log("earthquakeArray", earthquakeArray);
 
-    // Loop through the earthquakeArray and create one marker for each earthquake, bind a popup containing its title, and add it to the map.
+    // Loop through the earthquakeArray and create one circle marker for each earthquake
     for (let i = 0; i < earthquakeArray.length; i++) {
         let earthquake = earthquakeArray[i];
-        L.marker(earthquake.coordinates) // Fix the typo here and reference correctly
-            .bindPopup(`<h1>${earthquake.title}</h1>`)
+        let markerRadius = earthquake.magnitude * 8; // Adjust multiplier as necessary
+
+        L.circleMarker(earthquake.coordinates, {
+            radius: markerRadius, // Use the magnitude to determine the radius
+            color: 'blue', // Example color, change as needed
+            fillColor: getColor(earthquake.depth), // Call a function to determine color based on depth
+            fillOpacity: 0.6,
+            weight: 1
+        })
+            .bindPopup(`<h1>${earthquake.title}</h1> <hr> <h3>Magnitude: ${earthquake.magnitude}</h3> <h3>Depth: ${earthquake.depth}</h3>`)
             .addTo(myMap);
     }
+
 }
 
+function createLegend() {
+
+}
